3bcab940ec61b97839d978ff11d4d3296595da8b
lrelu <newlik@naver.com>
2019-01-28 09:17:01 +0900

    : feat - 라즈베리파이 서보모터 제어

--- _sources/0125_OpenAPI/Dump/Program.cs
+++ _sources/0125_OpenAPI/Dump/Program.cs
@@ -11,8 +11,7 @@ namespace Dump
 	{
 		public static void Main(string[] args)
 		{
-			string temp = MakeJson();
-			temp = NaverOpenAPI.FindLocation("도서관");
+			string temp = RunNaverAPI();
 			Console.WriteLine(temp);
 		}
 
@@ -25,28 +24,17 @@ namespace Dump
 			return json.ToString();
 		}
 
-		public static void RunNaverAPI()
+		public static string RunNaverAPI()
 		{
-			string url = "https://openapi.naver.com/v1/datalab/shopping/categories";
+			//string query = "손흥민";
+			string url = "http://www.google.com";
 			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-			request.Headers.Add("X-Naver-Client-Id", "hf5c3GitwQe5RNtxnlHz");
-			request.Headers.Add("X-Naver-Client-Secret", "p22c2JJzMj");
-			request.ContentType = "application/json";
-			request.Method = "POST";
-			string body = "{\"startDate\":\"2017-08-01\",\"endDate\":\"2017-09-30\",\"timeUnit\":\"month\",\"category\":[{\"name\":\"패션의류\",\"param\":[\"50000000\"]},{\"name\":\"화장품/미용\",\"param\":[\"50000002\"]}],\"device\":\"pc\",\"ages\":[\"20\",\"30\"],\"gender\":\"f\"}";
-			byte[] byteDataParams = Encoding.UTF8.GetBytes(body);
-			request.ContentLength = byteDataParams.Length;
-			Stream st = request.GetRequestStream();
-			st.Write(byteDataParams, 0, byteDataParams.Length);
-			st.Close();
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 			Stream stream = response.GetResponseStream();
 			StreamReader reader = new StreamReader(stream, Encoding.UTF8);
 			string text = reader.ReadToEnd();
-			stream.Close();
-			response.Close();
-			reader.Close();
-			Console.WriteLine(text);
+
+			return text;
 		}
 	}
 }
